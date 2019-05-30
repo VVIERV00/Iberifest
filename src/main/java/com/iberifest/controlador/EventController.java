6ba4e1fc8a3dff20504d872ef5f0821f16ba2cd9
Gonzalo Molpeceres <gonzalomolbar@gmail.com>
2019-05-30 12:42:28 +0200

    Mapa de busqueda de eventos completado

--- src/main/java/com/iberifest/controlador/EventController.java
+++ src/main/java/com/iberifest/controlador/EventController.java
@@ -33,12 +33,16 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.primefaces.event.map.PointSelectEvent;
+import org.primefaces.event.map.StateChangeEvent;
 
 import org.primefaces.model.map.DefaultMapModel;
 import org.primefaces.model.map.LatLng;
+import org.primefaces.model.map.LatLngBounds;
 import org.primefaces.model.map.MapModel;
 import org.primefaces.model.map.Marker;
 
@@ -64,10 +68,11 @@ public class EventController implements Serializable {
     private MapModel simpleModel;
     private LatLng coord;
     private HashMap<Integer, MapModel> hashMaps;
-    
+
     private static String API_KEY = "AIzaSyCT5Gp11FBLtymzALIDbF0AG6Gwvtl27E0";
     private double maxDistancia;
     private String coordenadasOrigenTexto;
+    private String coordenadasOrigen;
 
     @PostConstruct
     public void init() {
@@ -79,15 +84,16 @@ public class EventController implements Serializable {
         hashMaps = new HashMap<>();
         maxDistancia = 20;
         coordenadasOrigenTexto = "";
+        coordenadasOrigen = "41.9792243,-6.0599661";
+
     }
 
     public void searchEventByFilter() {
         FacesMessage message = null;
-        
-        String coordenadasOrigen = getCoordinatesFromString(coordenadasOrigenTexto);
-        System.out.println(maxDistancia+" "+coordenadasOrigenTexto+" "+coordenadasOrigen);
-        
-        
+
+        coordenadasOrigen = getCoordinatesFromString(coordenadasOrigenTexto);
+        System.out.println(maxDistancia + " " + coordenadasOrigenTexto + " " + coordenadasOrigen);
+
         if (!user.getUsername().equals("")) {
             User userAux = userEJB.getByUserFullUserName(user);
 
@@ -110,11 +116,13 @@ public class EventController implements Serializable {
 
     public String getCoordinatesFromString(String address) {
 
+        if (address.equals("")) {
+            return null;
+        }
         address = address.replace(" ", "+");
         JSONObject json;
-        String lng,lat,coordenadas;
-        
-                
+        String lng, lat, coordenadas;
+
         try {
             json = readJsonFromUrl("https://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&key=" + API_KEY + "");
             lat = json.getJSONArray("results").getJSONObject(0).getJSONObject("geometry").getJSONObject("location").get("lat").toString();
@@ -125,10 +133,26 @@ public class EventController implements Serializable {
             return null;
         }
 
-       
-
         return coordenadas;
         //System.out.println(json.);
+    }
+
+    public String getAddressFromCoordinates(String coordinates) {
+
+        JSONObject json;
+        String lng, lat, coordenadas;
+        String address = "";
+        try {
+            json = readJsonFromUrl("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + coordinates + "&key=" + API_KEY + "");
+            //address = json.getJSONArray("results").getJSONObject(0).getJSONObject("geometry").getJSONObject("location").get("lat").toString();
+            address = json.getJSONArray("results").getJSONObject(1).getString("formatted_address");
+        } catch (Exception ex) {
+            Logger.getLogger(EventController.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        System.out.println(address);
+        return address;
+
     }
 
     public static double calcularDistancia(String coordenadas1, String coordenadas2) {
@@ -154,7 +178,7 @@ public class EventController implements Serializable {
     }
 
     public void renderCoordinates() {
-
+        simpleModel = new DefaultMapModel();
         for (Event e : listaEventos) {
             String coordenadas[] = e.getCoordinates().split(",");
             String lat = coordenadas[0];
@@ -164,13 +188,75 @@ public class EventController implements Serializable {
             coord = new LatLng(latD, lngD);
 
             //Basic marker
-            simpleModel = new DefaultMapModel();
-
             simpleModel.addOverlay(new Marker(coord, "Localizacion"));
 
-            hashMaps.put(e.getId_event(), simpleModel);
+            //hashMaps.put(e.getId_event(), simpleModel);
         }
 
+    }
+    
+        public void renderAllCoordinates() {
+        simpleModel = new DefaultMapModel();
+        List<Event> allEvents = eventEJB.findAll();
+        for (Event e : allEvents) {
+            String coordenadas[] = e.getCoordinates().split(",");
+            String lat = coordenadas[0];
+            String lng = coordenadas[1];
+            double latD = Double.parseDouble(lat);
+            double lngD = Double.parseDouble(lng);
+            coord = new LatLng(latD, lngD);
+
+            //Basic marker
+            simpleModel.addOverlay(new Marker(coord, "Localizacion"));
+
+            //hashMaps.put(e.getId_event(), simpleModel);
+        }
+
+    }
+
+    String getMediaCoordinates(List<Event> events) {
+        Double lng = 0.0, lat = 0.0;
+        int x = 0;
+        for (Event event : events) {
+            x++;
+            lat += Double.valueOf(event.getCoordinates().split(",")[0]);
+            lng += Double.valueOf(event.getCoordinates().split(",")[1]);
+
+        }
+
+        lat = lat / x;
+        lng = lng / x;
+
+        return lat + "," + lng;
+    }
+
+    public void onPointSelect(PointSelectEvent event) {
+        LatLng latlng = event.getLatLng();
+
+        //FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Point Selected", "Lat:" + latlng.getLat() + ", Lng:" + latlng.getLng()));
+        String lat = String.valueOf(latlng.getLat());
+        String lng = String.valueOf(latlng.getLng());
+        String coordenadas = lat + "," + lng;
+        coordenadasOrigenTexto = getAddressFromCoordinates(coordenadas);
+    }
+
+    public void addMessage(FacesMessage message) {
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+
+    public String getCoordenadasOrigen() {
+
+        if (!listaEventos.isEmpty()) {
+            coordenadasOrigen = getMediaCoordinates(listaEventos);
+        }else{
+            renderAllCoordinates();
+        }
+        
+        return coordenadasOrigen;
+    }
+
+    public void setCoordenadasOrigen(String coordenadasOrigen) {
+        this.coordenadasOrigen = coordenadasOrigen;
     }
 
     public double getMaxDistancia() {
@@ -188,7 +274,7 @@ public class EventController implements Serializable {
     public void setCoordenadasOrigenTexto(String coordenadasOrigenTexto) {
         this.coordenadasOrigenTexto = coordenadasOrigenTexto;
     }
-    
+
     public LatLng getCoord() {
         return coord;
     }
