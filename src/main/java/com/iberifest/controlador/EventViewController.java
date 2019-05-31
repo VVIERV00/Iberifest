b9bb7b565dbf6c64bf45b20c2ef56da058b4d6f9
Adolfo Jiménez <adolfojimenezpr@gmail.com>
2019-05-31 11:28:49 +0200

    Funcionando el paso de parametro a otra vista, procedemos a desarrollar la vista con comentarios y denuncias.

--- src/main/java/com/iberifest/controlador/EventViewController.java
+++ src/main/java/com/iberifest/controlador/EventViewController.java
@@ -1,19 +1,56 @@
 package com.iberifest.controlador;
 
+import com.iberifest.EJB.EventFacadeLocal;
+import com.iberifest.modelo.Event;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import java.io.Serializable;
 
+import java.util.Map;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.context.FacesContext;
+
 @ManagedBean
 @SessionScoped
 public class EventViewController implements Serializable {
-    private String idEvento;
 
-    public String getIdEvento() {
+    @EJB
+    private EventFacadeLocal eventEJB;
+
+    private int idEvento;
+    private Event evento;
+
+    
+
+    public void searchEventDetails(int id) {
+        
+        evento = eventEJB.find(id);
+        System.out.println(evento.getDescription());
+    }
+
+    public int getIdEvento() {
+        int id;
+        FacesContext fc = FacesContext.getCurrentInstance();
+        Map<String, String> params
+                = fc.getExternalContext().getRequestParameterMap();
+        String data = params.get("idEvento");
+        idEvento = Integer.parseInt(data);
+        searchEventDetails(idEvento);
+
         return idEvento;
     }
 
-    public void setIdEvento(String idEvento) {
+    public void setIdEvento(int idEvento) {
         this.idEvento = idEvento;
     }
+
+    public Event getEvento() {
+        return evento;
+    }
+
+    public void setEvento(Event evento) {
+        this.evento = evento;
+    }
+
 }
